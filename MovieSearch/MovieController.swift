37ad813b67ce2bcdec929fc8f7d735d16ed08402
Steve Cox <coxsteve@gmail.com>
2016-06-03 14:57:49 -0600

    heavily dependent on Pokemon, set up MovieContrler

--- MovieSearch/MovieController.swift
+++ MovieSearch/MovieController.swift
@@ -7,3 +7,33 @@
 //
 
 import Foundation
+
+class MovieController {
+
+    typealias JSON = [String: AnyObject]
+    
+    static let sharedController = MovieController()
+    
+    let baseUrl = NSURL(string: "https://api.themoviedb.org/3/movie/")
+    
+    static let keyForAPI = "?api_key=f83783c7c1e09d03fe09770bc9c4bf57"
+    
+    var movies: [Movie] = []
+    
+    func getMovies(completion: (movies: [Movie]) -> Void ) {
+        guard let unwrappedURL = baseUrl else {
+        return }
+        
+    NetworkController.performRequestForURL(unwrappedURL, httpMethod: .Get) { (data, error) in
+        guard let data = data,
+        jsonDictionary = (try? NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments)),
+        resultsArray = jsonDictionary["results"] as? [JSON]
+            else {
+                completion(movies: [])
+                return
+        }
+        let moviesArray = resultsArray.flatMap({Movie(jsonDictionary:$0)})
+        completion(movies: moviesArray)
+        }
+    }
+}
