c00457c707a3efa335abce8f4070b7419b1c738c
Steve Cox <coxsteve@gmail.com>
2016-06-10 10:32:37 -0600

    got baseURL set in MC, completed Model

--- MovieSearch/Movie.swift
+++ MovieSearch/Movie.swift
@@ -7,3 +7,36 @@
 //
 
 import Foundation
+
+typealias JSON = [String: AnyObject]
+
+struct Movie {
+    
+    private let kId = "id"
+    private let kTitle = "original_title"
+    private let kOverview = "overview"
+    private let kAvg = "vote_average"
+    private let kPosterPath = "poster_path"
+    
+    let identifier: Int
+    let title: String
+    let overview: String
+    let voteAverage: Double
+    let posterPathURL: NSURL
+    
+    init?(json: JSON) {
+        
+        guard let identifier = json[kId] as? Int,
+              let title = json[kTitle] as? String,
+              let overview = json[kOverview] as? String,
+              let voteAverage = json[kAvg] as? Double,
+              let posterPathURLStr = json[kPosterPath] as? String,
+              let posterPathURL = NSURL(string:"http://image.tmdb.org/t/p/w500/\(posterPathURLStr)") else
+            { return nil }
+        self.identifier = identifier
+        self.title = title
+        self.overview = overview
+        self.voteAverage = voteAverage
+        self.posterPathURL = posterPathURL
+    }
+}
--- MovieSearch/MovieController.swift
+++ MovieSearch/MovieController.swift
@@ -7,3 +7,16 @@
 //
 
 import Foundation
+
+class MovieController {
+    
+    static let sharedController = MovieController()
+    
+    //combined URL for my sanity:  https://api.themoviedb.org/3/search/movie?api_key=f83783c7c1e09d03fe09770bc9c4bf57&query= (then the inquiry entered by the user)
+    
+    static let baseURL = NSURL(string: "http://api.themoviedb.org/3/search/movie") ?? NSURL()
+    
+    static let apiKey = "f83783c7c1e09d03fe09770bc9c4bf57"
+
+
+}
